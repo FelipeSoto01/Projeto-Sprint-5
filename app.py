ff6b4c5fb3a87174f993896849c1df6374d6aeb7
Felipe Soto <99318405+FelipeSoto01@users.noreply.github.com>
2025-05-14 13:24:52 -0300

    update 3

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
 # importando bibliotecas
-import streamlit as st
 import pandas as pd
 import plotly.express as px
+import streamlit as st
 
 
 # lendo o arquivo csv
-df_car_data = pd.read_csv('vehicles.csv')
-
-st.header('Vehiculos')
-
-hist_button = st.button('Criar histograma')
-
-if hist_button:
-    st.write('Criando histograma para conjunto de dados')
-
-    hist_df_car_data = px.histogram(df_car_data, x="odometer")
-
-    st.plotly_chart(hist_df_car_data, use_container_width=True)
+vehicles_data = pd.read_csv('vehicles.csv')
 
 
-scatter_button = st.button('Criar grafico de dispersao')
+def create_hist(data):
+    return px.histogram(data, x="odometer")
 
-if scatter_button:
-    st.write('Criando grafico de dispersao')
 
-    scatter_df_car_data = px.scatter(df_car_data, x="odometer", y="price")
+def create_scatter(data):
+    return px.scatter(data, x="odometer", y="price")
 
-    st.plotly_chart(scatter_df_car_data, use_container_width=True)
+
+st.header('Analise de Veiculos')
+
+choose_graph = st.selectbox('Selecione um tipo de grafico', [
+                            'Histograma', 'Grafico de dispersão'])
+
+if choose_graph == 'Histograma':
+    st.write('Criando histograma')
+    hist = create_hist(vehicles_data)
+    st.plotly_chart(hist, use_container_width=True)
+
+elif choose_graph == 'Grafico de dispersão':
+    st.write('Criando grafico de dispersão')
+    scatter = create_scatter(vehicles_data)
+    st.plotly_chart(scatter, use_container_width=True)
