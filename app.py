b300856de3e52e0d6917fd1c817cde91024ca65c
Felipe Soto <99318405+FelipeSoto01@users.noreply.github.com>
2025-05-14 16:43:26 -0300

    UPDATE!!

--- app.py
+++ app.py
@@ -1,32 +1,61 @@
-# importando bibliotecas
+# Importando bibliotecas
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
 
-# lendo o arquivo csv
-vehicles_data = pd.read_csv('vehicles.csv')
+# Carregando arquivo e prevendo erro
+try:
+    vehicles_data = pd.read_csv('vehicles.csv')
+except FileNotFoundError:
+    st.error("O arquivo 'vehicles.csv' não foi encontrado.")
+    st.stop()
+except pd.errors.EmptyDataError:
+    st.error("O arquivo está vazio.")
+    st.stop()
 
+
+# Funçoes para criar os graficos
 
 def create_hist(data):
-    return px.histogram(data, x="odometer")
+    """
+    Esta funçao recebe um dataframe como argumento e 
+    cria um histograma de quilometragem, com titulo e nomeando o eixo x.
+    """
+    return px.histogram(
+        data, x="odometer", title='Distribuição de KM',
+        labels={"odometer": "Quilometragem (KM)"}
+    )
 
 
 def create_scatter(data):
-    return px.scatter(data, x="odometer", y="price")
+    """
+    Esta funçao recebe um dataframe como argumento e
+    cria um grafico de dispersao entre quilometragem e
+    preço, adiciona titulo e nomeia os eixos
+    """
+    return px.scatter(
+        data, x="odometer", y="price",
+        title='Relação entre KM e Preço',
+        labels={"odometer": "Quilometragem (KM)", "price": "Preço (USD)"}
+    )
 
 
-st.header('Analise de Veiculos')
+# Interface
+st.header('Análise de Veículos Usados')
 
-choose_graph = st.selectbox('Selecione um tipo de grafico', [
-                            'Histograma', 'Grafico de dispersão'])
+
+choose_graph = st.selectbox('Selecione um tipo de gráfico:', [
+    'Histograma', 'Gráfico de Dispersão'
+])
+
 
 if choose_graph == 'Histograma':
-    st.write('Criando histograma')
+    st.write('Visualizando o **Histograma de KM:**')
     hist = create_hist(vehicles_data)
     st.plotly_chart(hist, use_container_width=True)
 
-elif choose_graph == 'Grafico de dispersão':
-    st.write('Criando grafico de dispersão')
+elif choose_graph == 'Gráfico de Dispersão':
+    st.write('Visualizando o **Gráfico de Dispersão (Preço vs KM):**')
     scatter = create_scatter(vehicles_data)
     st.plotly_chart(scatter, use_container_width=True)
